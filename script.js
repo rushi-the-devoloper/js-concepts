796b829b50b8a41b9b710b3ec35a3ecdd0a14165
rushi-reactdev <rushikeshkale669@gmail.com>
2023-04-06 23:51:24 +0530

    javascript concepts

---
+++ script.js
@@ -0,0 +1,122 @@
+let fund = 7000;
+let vehicle = "bullet";
+let person = 2;
+if(fund>=7200&&vehicle=="bullet"&&person<1 ){
+    console.log("person can travel whole maharashtra");
+}
+else if(fund<6000&&vehicle=="bullet"&&person<2){
+ console.log("person can travel whole kokan")
+}
+else if (fund<5000&&vehicle=="fz"&&person<2){
+    console.log("person can travel whole mumbai")
+}
+else if(fund<4000&&vehicle=="shine"&&person<2){
+    console.log("person can travel till mumbai from pune")
+}
+else(
+    "person should stay at home"
+)
+// if else if second example 
+let days = "monday";
+if(days==="sunday"){
+    console.log("wake up at 9am");
+}
+else if(day==="monday"){
+    console.log("wake up at 6 am ")
+}
+else if(day==="tuesday"){
+    console.log("wake up at 5 am")
+}
+else if(
+    day=== wedsday
+){
+    console.log("wake up at 4 am")
+}
+else if (day==="thursday"){
+    console.log("wake up at 3 am")
+}
+else{ console.log("dont wake up before 10am")
+
+}
+// //let day = 2;
+//switch (day){
+  // // case 0 : 
+   // console.log("sunday");
+   // break;
+   // case 1 : 
+   // console.log("monday");
+   // break;
+   // case 2 : 
+   // console.log("tuesday");
+   // break;
+   // case 3 : 
+   // console.log("wednesday");
+   // break;
+   // case 4 : 
+   // console.log("thursday");
+   // break;
+   // case 5 : 
+    //console.log("friday");
+    //break;
+    //default : console.log("its a off day take a break");
+
+let day = "friday";
+if(day==="sunday"){
+console.log("roj khao unday")
+}
+else if(day==="monday"){
+console.log("wake up at 4 am")
+}
+else if(day==="tuesday"){
+console.log("wake up at 5 am")
+}
+else if(day==="wednsday"){
+console.log("wake up at 6 am")
+}
+else if(day==="thursday"){
+console.log("wake up at 7 am")
+}
+else if(day==="friday"){
+console.log("wake up at 8 am")
+}
+else if(day==="saturday"){
+console.log("wake up at 9am")
+}
+
+// 
+// switch case statement
+let fruit = "banana";
+switch(fruit){
+    case "banana" :
+    console.log("gives energy to do work");
+
+    case "orange":
+        console.log("good for skin");
+    
+        case "pineapple":
+            console.log("very tasty");
+
+            case "apple":
+                console.log("eat an apple a day keep a doctor away");
+            
+                dafault:
+                console.log("please try to eat fruits as much as you can");
+}
+// 
+
+
+let total = 0;
+let num = 10;
+for(let i = 1; i<=num; i++){
+    total= total+i;
+}
+console.log(total);
+// i dont understand this suppose
+
+// that means
+// when first time loop runs 1+1=2;
+// second time 
+
+
+
+
